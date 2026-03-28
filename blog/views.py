e27364de0460b2a5efadf43da616834605f1281f
Priyanka Priyadarshini <priyankapriyadarshini091@gmail.com>
2026-03-28 21:40:22 +0530

    created canonical and SEO friendly URLS for posts

--- blog/views.py
+++ blog/views.py
@@ -9,11 +9,14 @@ def post_list(request):
         {'posts': posts}
     )
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISH
+        status=Post.Status.PUBLISH,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
     )
     return render(
         request,
